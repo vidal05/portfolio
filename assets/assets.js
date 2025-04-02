ef080db09b454c04af7066e5d3e0a21856c3b490
vidal05 <vidfloatson@gmail.com>
2025-04-01 19:04:52 -0600

    add services section

--- assets/assets.js
+++ assets/assets.js
@@ -103,9 +103,8 @@ export const workData = [
 
 export const serviceData = [
     { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
-    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
+    { icon: assets.mobile_icon, title: 'Data Science', description: 'Extracting and Visualizing info to increase business value', link: '' },
+    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' }
 ]
 
 export const infoList = [
